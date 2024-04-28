0f7ffc76611d8b1be3159f947fb53c05d1e97d6d
Dione V. Silva <135567312+Dionepx@users.noreply.github.com>
2024-04-27 22:09:46 -0300

    Add files via upload

---
+++ obj, array, chave e valor/script.js
@@ -0,0 +1,23 @@
+// const objeto = {} (bigodes é o simbolo do objeto)
+// "array": [] (colchetes é o simbolo do array)
+// "chave": "valor", 
+const data = {
+     "frutas": [
+          {
+               "nome": "Banana",
+               "preco": 1.40,
+               "quantidade": 40
+          },
+          {
+               "nome": "Laranja",
+               "preco": 2.30,
+               "quantidade": 40
+          },
+          {
+               "nome": "Morango",
+               "preco": 2.90,
+               "quantidade": 40
+          },
+     ],
+     "saldo": 0,
+};
